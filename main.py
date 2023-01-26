ed8cb7e1c00d618186d572e7af4feaa9e7d2e56c
emmanuel-mwendwa <mwendwae054@gmail.com>
2023-01-26 07:57:25 +0300

    A Portfolio website with flask and html

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, redirect, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/contact_me")
+def contact_me():
+    return render_template("contact_me.html")
+
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
